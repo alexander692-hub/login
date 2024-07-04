ae561cae399411c76ceab3632222eafd7db24fce
Edgardo <laraedgardo134@gmail.com>
2024-07-03 22:58:45 -0300

    actualizando sistema para login

---
+++ piedepagina.php
@@ -0,0 +1,29 @@
+			<div class="footer">
+				<div class="footer-inner">
+					<div class="footer-content">
+						<span class="bigger-120">
+							  <span class="blue bolder"> Equipo De Servidores K </span> &copy; <?=date('Y')?>
+						</span>
+
+						&nbsp; &nbsp;
+						<span class="action-buttons">
+							<a href="https://www.youtube.com/@EbenezerHon" target="_blank">
+								<i class="ace-icon fa fa-youtube-square red bigger-150"></i>
+							</a>
+							
+							<a href="https://www.facebook.com/ServidoresK" target="_blank">
+								<i class="ace-icon fa fa-facebook-square text-primary bigger-150"></i>
+							</a>
+							<!--<a href="https:/www.twitter.com/jasingafi" target="_blank">
+								<i class="ace-icon fa fa-twitter-square light-blue bigger-150"></i>
+							</a> -->
+
+							<a href="https://www.instagram.com/ebenezer_hn/" target="_blank">
+							<i class="ace-icon fa fa-instagram red bigger-150"></i>
+							</a>
+
+							
+						</span>
+					</div>
+				</div>
+			</div>
